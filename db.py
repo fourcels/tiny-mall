4f5598033d3e8d27b86e1796517bc1c788d21b8f
Kiyan Gauss <fourcels@gmail.com>
2021-11-26 16:12:18 +0800

    db init

--- db.py
+++ db.py
@@ -2,8 +2,16 @@ from tiny_mall.database import SessionLocal
 from tiny_mall.config import settings
 from tiny_mall import cruds, schemas
 
+import click
 
-def init_db():
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def init_admin():
     with SessionLocal() as db:
         db_user = cruds.user.get_user_by_username(db, settings.admin_username)
         if not db_user:
@@ -12,3 +20,7 @@ def init_db():
                 password=settings.admin_password,
             )
             cruds.user.create_user(db, user, True)
+
+
+if __name__ == '__main__':
+    cli()
